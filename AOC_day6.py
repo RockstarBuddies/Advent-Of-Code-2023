f97a440afd649328c6e5781a6f7983e1a147a9f3
Aditya Gupta <84505010+RockstarBuddies@users.noreply.github.com>
2023-12-08 14:32:17 +0530

    Update AOC_day6.py

--- AOC_day6.py
+++ AOC_day6.py
@@ -1,8 +1,10 @@
 import math
+#Part 1
 print(math.prod(sum(i * (limit - i) > distance for i in range(distance)) for limit, distance in zip([62, 73, 75, 65], [644, 1023, 1240, 1023])))
 
-#limit, distance = 62737565,644102312401023
-#print(sum(i * (limit - i) > distance for i in range(limit)))
+#Part 2
+limit, distance = 62737565,644102312401023
+print(sum(i * (limit - i) > distance for i in range(limit)))
 
 
 
