c8fdc478ebc2040065df37bb4e9076e5ee18ca50
Aditya Gupta <84505010+RockstarBuddies@users.noreply.github.com>
2023-12-15 00:07:58 +0530

    Update AOC_day14.py

--- AOC_day14.py
+++ AOC_day14.py
@@ -1,3 +1,18 @@
+#Part 1
+def move(d):
+    for _ in range(100):
+        d = map(lambda s: s.replace('.O', 'O.'), d)
+    return d
+
+def tilt(d):
+    return map(''.join, zip(*d))
+
+def load(d):
+    return sum(i*(c=='O') for col in d
+        for i,c in enumerate(col[::-1], 1))
+
+print(load(move(tilt(open('input14.txt')))))
+#Part 2
 lines = [list(line.strip()) for line in open('input14.txt')]
 def move(lines):
     for _ in range(len(lines)):
